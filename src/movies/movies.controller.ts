81408eebf21beb8bbb32f2ed92e05413fce20c7f
Tae Hyun Lee <40256745+lth9710@users.noreply.github.com>
2022-08-27 16:30:18 +0900

    0827

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+
+    @Get()
+    getAll(){
+        return 'This will return all movies';
+    }
+    @Get('/:id')
+    getOne(@Param("id") movieId:string) {
+        return `This will return one movie with the id: ${movieId}`;
+    }
+    
+
+    @Post()
+    create(){
+        return 'This will create a movie';
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') movieId:string) {
+        return `This will delete a movie ${movieId}`;
+    }
+
+    @Patch('/:id')
+    patch(@Param('id') movieId:string){
+        return `This will patch one movie with the id: ${movieId}`;
+    }
+
+
+}
+
+    
