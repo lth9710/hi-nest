5a5ae5cefcc4188dcb7f20e9a82428bbb60a972c
Tae Hyun Lee <40256745+lth9710@users.noreply.github.com>
2022-08-30 20:43:24 +0900

    0830

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,11 +1,17 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+
 
 @Controller('movies')
 export class MoviesController {
 
+    constructor(private readonly moviesService: MoviesService) {}
+
     @Get()
-    getAll(){
-        return 'This will return all movies';
+    getAll() : Movie[]{
+        return this.moviesService.getAll();
     }
 
     @Get('search')
@@ -15,19 +21,19 @@ export class MoviesController {
     
     
     @Get(':id')
-    getOne(@Param("id") movieId:string) {
-        return `This will return one movie with the id: ${movieId}`;
+    getOne(@Param("id") movieId:string):Movie {
+        return this,this.moviesService.getOne(movieId)
     }
     
 
     @Post()
     create(@Body() movieData){    
-        return movieData;
+        return this.moviesService.create(movieData);
     }
 
     @Delete('/:id')
     remove(@Param('id') movieId:string) {
-        return `This will delete a movie ${movieId}`;
+        return this.moviesService.deleteOne(movieId);
     }
 
     @Patch('/:id')
