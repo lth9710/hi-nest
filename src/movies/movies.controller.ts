1342300c8eff3a3dbea2caa614c85ca26807c923
Tae Hyun Lee <40256745+lth9710@users.noreply.github.com>
2022-09-02 16:23:23 +0900

    0902

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -39,7 +40,7 @@ export class MoviesController {
     }
 
     @Patch('/:id')
-    patch(@Param('id') movieId:number, @Body() updateData){
+    patch(@Param('id') movieId:number, @Body() updateData: UpdateMovieDto){
         return this.moviesService.update(movieId, updateData);        
     }
    
